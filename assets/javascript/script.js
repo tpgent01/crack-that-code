1ba39a7b3646706219ff92dcce0d622a50426f5b
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-14 14:06:15 -0600

    creating js variables and quiz questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,47 @@
+// Variables w/ array and object for questions
+let questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+
+// Score & Questions Variables
+let score = 0;
+let questionsIndex = 0;
+
+
+// Quiz Timer Variables
+let currentTime = document.querySelector("#currentTime");
+let timer = document.querySelector("#startBtnEl");
+let questionsEl = document.querySelector("#questionsEl");
+let wrapper = document.querySelector("#wrapper");
+let secondsLeft = 76;
+let holdInterval = 0;
+let pentalty = 10;
+let ulCreate = document.createElement("ul");
+
+
+// Timer Function
